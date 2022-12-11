79740181b2d8b899de58b2512b9f39a230cd346e
Przemo12121 <49036565+Przemo12121@users.noreply.github.com>
2022-12-11 16:08:03 +0100

    sln files automatic adjsutment

--- DevicesManagement/DevicesManagement/Controllers/DevicesController.cs
+++ DevicesManagement/DevicesManagement/Controllers/DevicesController.cs
@@ -2,53 +2,52 @@
 using Microsoft.AspNetCore.Mvc;
 using Database.Models.Interfaces;
 
-namespace DevicesMenagement.Controllers
+namespace DevicesMenagement.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DevicesController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class DevicesController : ControllerBase
+    [HttpGet, Route("/")]
+    public List<IUser> ListAllDevices(int? limit, int? offset, string? order)
     {
-        [HttpGet, Route("/")]
-        public List<IUser> ListAllDevices(int? limit, int? offset, string? order)
-        {
-            // endpoint will be used to list brief information about all devices
-            return new List<IUser>();
-        }
+        // endpoint will be used to list brief information about all devices
+        return new List<IUser>();
+    }
 
-        [HttpGet, Route("/:employeeId")]
-        public List<IUser> ListUserDevices(int employeeId)
-        {
-            // endpoint will be used to list brief information about requesting user's devices
-            return new List<IUser>();
-        }
+    [HttpGet, Route("/:employeeId")]
+    public List<IUser> ListUserDevices(int employeeId)
+    {
+        // endpoint will be used to list brief information about requesting user's devices
+        return new List<IUser>();
+    }
 
-        [HttpGet, Route("/:deviceId/commands")]
-        public List<ICommand> ListDeviceCommands(int deviceId, int? limit, int? offset, string? order)
-        {
-            // return commands registered to device
-            return new List<ICommand>();
-        }
+    [HttpGet, Route("/:deviceId/commands")]
+    public List<ICommand> ListDeviceCommands(int deviceId, int? limit, int? offset, string? order)
+    {
+        // return commands registered to device
+        return new List<ICommand>();
+    }
 
-        [HttpPost, Route("/")]
-        public IDevice RegisterDevice(IDevice device)
-        {
-            // adds new device
-            throw new NotImplementedException();
-        }
+    [HttpPost, Route("/")]
+    public IDevice RegisterDevice(IDevice device)
+    {
+        // adds new device
+        throw new NotImplementedException();
+    }
 
-        [HttpPost, Route("/:deviceId/commands")]
-        //!!! tutaj musze sie jeszcze pobawic w wysylanie i bindowanie requestow wiec beda parametry przerobione
-        public ICommand RegisterCommand(int deviceId, ICommand command)
-        {
-            // then send create new command
-            throw new NotImplementedException();
-        }
+    [HttpPost, Route("/:deviceId/commands")]
+    //!!! tutaj musze sie jeszcze pobawic w wysylanie i bindowanie requestow wiec beda parametry przerobione
+    public ICommand RegisterCommand(int deviceId, ICommand command)
+    {
+        // then send create new command
+        throw new NotImplementedException();
+    }
 
-        [HttpDelete, Route("/:deviceId")]
-        public string DeleteDevice(int deviceId)
-        {
-            // delete device
-            return "Not yet implemented";
-        }
+    [HttpDelete, Route("/:deviceId")]
+    public string DeleteDevice(int deviceId)
+    {
+        // delete device
+        return "Not yet implemented";
     }
 }
