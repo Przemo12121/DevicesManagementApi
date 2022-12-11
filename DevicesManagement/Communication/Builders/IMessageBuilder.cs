c2ca6dcae93eeeab3de746a5815be5af007b59ec
Przemo12121 <49036565+Przemo12121@users.noreply.github.com>
2022-12-11 16:06:54 +0100

    finished restructuring solution

---
+++ DevicesManagement/Communication/Builders/IMessageBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Communication;
+
+namespace Communication.Builders;
+
+/// <summary>
+/// Used to build requested message in requested format.
+/// </summary>
+/// <typeparam name="T">Type of message</typeparam>
+/// <typeparam name="U">Type of message content</typeparam>
+public interface IMessageBuilder<T, U> where T : IMessage<U>
+{
+    public string Build(T message);
+
+    public T Build(string message);
+}
