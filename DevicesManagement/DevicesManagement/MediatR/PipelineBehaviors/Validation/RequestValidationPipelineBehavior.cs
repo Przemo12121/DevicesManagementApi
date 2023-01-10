321fa9ad708619523034568c2f5b6948f03b694b
Przemo12121 <49036565+Przemo12121@users.noreply.github.com>
2023-01-10 15:58:35 +0100

    added BadRequestResponse, added mapping errors grouped by property name

--- DevicesManagement/DevicesManagement/MediatR/PipelineBehaviors/Validation/RequestValidationPipelineBehavior.cs
+++ DevicesManagement/DevicesManagement/MediatR/PipelineBehaviors/Validation/RequestValidationPipelineBehavior.cs
@@ -1,11 +1,15 @@
-﻿using FluentValidation;
+﻿using DevicesManagement.MediatR.Commands;
+using DevicesManagement.DataTransferObjects.Responses;
+using FluentValidation;
+using FluentValidation.Results;
 using MediatR;
+using System.Reflection.Metadata.Ecma335;
 
 namespace DevicesManagement.MediatR.PipelineBehaviors.Validation;
 
 public class RequestValidationPipelineBehavior<T, TValidator, TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     where TValidator : IValidator<T>
-    where TRequest : IRequest<TResponse>
+    where TRequest : IRequest<TResponse>, IRequestContainerCommand<T>
 {
     protected IEnumerable<IValidator<T>> Validators { get; init; }
 
@@ -23,6 +27,30 @@ public class RequestValidationPipelineBehavior<T, TValidator, TRequest, TRespons
 
     public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
+        var result = Validate(request.Request);
+        if (!result.IsValid)
+        {
+            throw new BadRequestResponse(GroupErrorsByProperty(result.Errors));
+        }
+
         throw new NotImplementedException();
     }
+
+    protected ValidationResult Validate(T request)
+    {
+        var errors = Validators.SelectMany(validator => validator.Validate(request).Errors)
+            .Where(errors => errors != null)
+            .ToList();
+
+        return new ValidationResult(!errors.Any(), errors);
+    }
+
+    protected record ValidationResult(bool IsValid, IEnumerable<ValidationFailure> Errors);
+
+    protected IEnumerable<KeyValuePair<string, IEnumerable<string>>> GroupErrorsByProperty(IEnumerable<ValidationFailure> errors)
+        => errors.GroupBy(x => x.PropertyName)
+            .Select(grouped => KeyValuePair.Create(
+                grouped.First().PropertyName,
+                grouped.Select(x => x.ErrorMessage)
+            ));
 }
